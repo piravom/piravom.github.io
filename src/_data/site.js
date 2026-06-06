8cafe61f54914866e775131721e62f104c5c55b5
Shajan Jacob <shajanjp@gmail.com>
2026-06-06 14:30:23 +0530

    feat: updates authors

--- src/_data/site.js
+++ src/_data/site.js
@@ -2,7 +2,10 @@ module.exports = {
   name: 'Piravom',
   description: 'Town portal for Piravom',
   url: 'https://piravom.in',
-  author: 'Contributors',
+  author: [
+    { name: 'Shajan', url: 'https://instagram.com/shajanjp' },
+    { name: 'Emil', url: 'https://instagram.com/emilrosemathew' },
+  ],
   phone: '+91 9876543210',
   email: 'mailpiravom@gmail.com',
   social: {
